9cb23abdf46339d9cccc3b1ae36eed24f3aa20a6
kvzhao <kevinzhaio@gmail.com>
2014-02-18 23:44:35 +0800

    Simple Integral operation

--- IMU_Navigation.ino
+++ IMU_Navigation.ino
@@ -1,5 +1,7 @@
 
-int val_x;
+unsigned int a_x;
+int vx, vx0;
+unsigned long cur_time, prev_time;
 
 void setup()
 {
@@ -9,12 +11,23 @@ void setup()
     // Built-in reference, 3.3V
     analogReference(AR_DEFAULT);
     // Due has a 12-bit ADC, this will return the
-    // value between 0 and 1023
+    // value between 0 and 4096
     analogReadResolution(12);
+
+    // Set initial velocity to zero
+    vx0 = 0;
+    // time tick
+    cur_time = millis();
+    prev_time = millis();
 }
 
 void loop()
 {
-    val_x = analogRead(A0);
-    Serial.println(val_x);
+    cur_time = millis();
+    a_x = analogRead(A0);
+    vx  = vx0 + a_x * (prev_time - cur_time);
+    Serial.println(vx);
+
+    prev_time = cur_time;
 }
+
